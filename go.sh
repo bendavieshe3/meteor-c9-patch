df2cec40be70bb706005f2c983bc973248a70258
bendavies <bendavies@he3.com.au>
2013-02-25 22:43:49 +1000

    Updated Go script to remove patch files after use

--- go.sh
+++ go.sh
@@ -1,5 +1,15 @@
+echo 
+echo '---------------------------'
 echo 'Patching Meteor for Cloud 9'
+echo '---------------------------'
+echo 
+
 curl https://raw.github.com/bendavieshe3/meteor-c9-patch/master/c9-meteor.patch > c9-meteor.patch
 curl https://raw.github.com/bendavieshe3/meteor-c9-patch/master/patch-for-c9.sh > patch-for-c9.sh
+
 chmod +x patch-for-c9.sh
 ./patch-for-c9.sh
+
+rm ./patch-for-c9.sh
+rm ./c9-meteor.path
+
